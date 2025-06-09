1348f137f81afefa7d4b7c25219964b86f387c09
Daniel Lemire <daniel@lemire.me>
2025-06-09 15:56:06 -0400

    Modifying grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,28 @@ module.exports = function(grunt) {
                     file: 'amd/build/uteluqchatbot.min.js'
                 }
             }
+        },
+        clean: {
+            build: ['build/']
+        },
+        stylelint: {
+            options: {
+                configFile: '.stylelintrc',
+                formatter: 'string',
+                ignoreDisables: false,
+                failOnError: true,
+                outputFile: '',
+                reportNeedlessDisables: false,
+                syntax: ''
+            },
+            src: ['**/*.css', '**/*.scss']
         }
     });
 
     grunt.loadNpmTasks('grunt-rollup');
-    grunt.registerTask('default', ['rollup']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-stylelint');
+
+    grunt.registerTask('amd', []); // Dummy task for compatibility
+    grunt.registerTask('default', ['clean', 'stylelint']);
 };
